e584835a3f8cfca7085982d8133e3d8a95a111be
kutar37 <kutar37@gmail.com>
2018-09-03 15:47:34 +0900

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.target')
+    .on('mouseover', function () {
+        $(this).children('.photo').css({
+            'transform': 'scale(' + $(this).attr('data-scale') + ')'
+        });
+    })
+    .on('mouseout', function () {
+        $(this).children('.photo').css({
+            'transform': 'scale(1)'
+        });
+    })
+    .on('mousemove', function (e) {
+        $(this).children('.photo').css({
+            'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 + '%'
+        });
+    })
+    .append('<div class="photo"></div>')
+    .children('.photo').css({
+                'background-image': 'url(' + $('.target').attr('data-image') + ')'
+    })
